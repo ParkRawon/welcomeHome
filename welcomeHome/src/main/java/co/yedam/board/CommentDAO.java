8f14356a3da0e5f55fce14098c136c7865b0bc3c
DESKTOP-7UNL8CG\admin <dkw0219753@daum.net>
2021-08-03 16:56:12 +0900

    8/3

--- welcomeHome/src/main/java/co/yedam/board/CommentDAO.java
+++ welcomeHome/src/main/java/co/yedam/board/CommentDAO.java
@@ -17,6 +17,89 @@ public class CommentDAO extends DAO {
 		instance = new CommentDAO();
 		return instance;
 	}
+	
+	
+	//글내용수정
+		public HashMap<String, Object> update(Comment comment) {	
+			connect();
+			try {
+				pstmt = conn.prepareStatement("update comments set name=?, content=? where id=?");
+				pstmt.setString(1, comment.getName());
+				pstmt.setString(2, comment.getContent());
+				pstmt.setString(3, comment.getId());
+				int r = pstmt.executeUpdate();
+				System.out.println("수정: "+ r);
+				
+				HashMap<String, Object> map = new HashMap<String, Object>();  
+				map.put("id", comment.getId());
+				map.put("name", comment.getName());
+				map.put("content", comment.getContent());
+
+				return map;
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				
+			} finally {
+				disconnect();
+				
+			}
+			return null;
+		}
+	
+	
+	// 글등록
+	public HashMap<String, Object> insert(Comment comment) {
+		// id_repository테이블에서 현재 시퀀스번호.
+		// comments 테이블에 추가.
+		// id_repositoty에 새로운 시퀀스번호로 변경.
+		int nextId = 0;
+		connect();
+		try {
+			conn.setAutoCommit(false);
+			// 1. 현재 시쿤스 번호 가져오기
+			stmt = conn.createStatement(); // pstmt ???
+			rs = stmt.executeQuery("select value from id_repository where name='comment'");
+			if (rs.next()) {
+				nextId = rs.getInt("value");
+			}
+			// 2. 시퀀스에 1을 증가시켜 comment에 입력
+			nextId++;
+			pstmt = conn.prepareStatement("insert into comments values(?,?,?)");
+			pstmt.setInt(1, nextId);
+			pstmt.setString(2, comment.getName());
+			pstmt.setString(3, comment.getContent());
+			int r = pstmt.executeUpdate(); // 조회: executeQuery, 입력수정삭제: executUpdate
+			System.out.println("입력건수: " + r);
+			// 3. 시퀀스번호를 변경
+			pstmt = conn.prepareStatement("update id_repository set value=? where name='comment'");
+			pstmt.setInt(1, nextId);
+			pstmt.executeUpdate();
+
+			conn.commit();
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("id", nextId);
+			map.put("name", comment.getName());
+			map.put("content", comment.getContent());
+
+			return map;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("msg", e.getMessage());
+				return map;
+
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} finally {
+			disconnect();
+		}
+		return null;
+	}
 
 	// 글목록
 	public List<HashMap<String, Object>> selectAll() {
@@ -39,4 +122,5 @@ public class CommentDAO extends DAO {
 		}
 		return list;
 	}
+		
 }
--- welcomeHome/src/main/java/co/yedam/board/CommentServlet.java
+++ welcomeHome/src/main/java/co/yedam/board/CommentServlet.java
@@ -19,7 +19,7 @@ public class CommentServlet extends HttpServlet {
 		super();
 
 	}
-
+    // 페이지 운영 방식 => html(화면) <--> java(서블릿,DAO) <-->  데이터베이스
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
@@ -29,32 +29,86 @@ public class CommentServlet extends HttpServlet {
 
 		String cmd = request.getParameter("cmd");
 		if (cmd == null) {
-			StringBuffer sb = new StringBuffer();
-			sb.append("<result>");
-			sb.append("<code>error</code>");
-			sb.append("<data>");
-			sb.append("cmd null");
-			sb.append("</data>");
-			sb.append("</result>");
-			out.print(sb.toString());
-		} else if (cmd.equals("selectAll")) {
-			List<HashMap<String, Object>> list = CommentDAO.getInstance().selectAll();
-			StringBuffer sb = new StringBuffer();
-			sb.append("<result>");
-			sb.append("<code>success</code>");
-			for (HashMap<String, Object> map : list) {
+			out.println(errorXML("cmd null"));
+
+		} else if (cmd.equals("selectAll")) {   // ===전체조회===
+			try {
+				List<HashMap<String, Object>> list = CommentDAO.getInstance().selectAll();
+				StringBuffer sb = new StringBuffer();
+				sb.append("<result>");
+				sb.append("<code>success</code>");
 				sb.append("<data>");
-				sb.append("<id>" + map.get("id") + "</id>");
-				sb.append("<name>" + map.get("name") + "</name>");
-				sb.append("<content>" + map.get("content") + "</content>");
+
+				for (HashMap<String, Object> map : list) {
+					sb.append("<row>");
+					sb.append(" <id>" + map.get("id") + "</id>");
+					sb.append(" <name>" + map.get("name") + "</name>");
+					sb.append(" <content>" + map.get("content") + "</content>");
+					sb.append("</row>");
+				}
 				sb.append("</data>");
+				sb.append("</result>");
+				out.print(sb.toString());
+
+			} catch (Exception e) {
+				out.println(errorXML(e.getMessage()));
 			}
-			sb.append("</result>");
-			out.print(sb.toString());
-		} else if (cmd.equals("insert")) {
 
+			
+		} else if (cmd.equals("insert")) {   // ===한건입력===
+			try {
+//				int result = 30/ 0;  오류확인
+				String name = request.getParameter("name");
+				String content = request.getParameter("content");
+				Comment comment = new Comment();
+				comment.setName(name);
+				comment.setContent(content);
+				HashMap<String, Object> map = CommentDAO.getInstance().insert(comment);
+				
+				out.println(dataXML(map));
+
+			} catch (Exception e) {
+				out.println(errorXML(e.getMessage()));
+			}
+		} else if (cmd.equals("update")) {     // ===수정===
+			String id = request.getParameter("id");
+			String name = request.getParameter("name");
+			String content = request.getParameter("content");
+			
+			Comment comment = new Comment();  
+			comment.setId(id);
+			comment.setName(name);
+			comment.setContent(content);
+			
+			HashMap<String, Object> map = CommentDAO.getInstance().update(comment);
+			
+			out.println(dataXML(map));
+			
 		}
+	}// end of doGet();
 
+	private String dataXML(HashMap<String, Object> map) {  //data정보
+		StringBuffer sb = new StringBuffer();
+		sb.append("<result>");
+		sb.append("<code>success</code>");
+		sb.append("<data>");
+		sb.append(" <id>" + map.get("id") + "</id>");
+		sb.append(" <name>" + map.get("name") + "</name>");
+		sb.append(" <content>" + map.get("content") + "</content>");
+		sb.append("</data>");
+		sb.append("</result>");
+
+		return sb.toString();
+
+	}
+
+	private String errorXML(String msg) {   //에러메세지
+		StringBuffer sb = new StringBuffer();
+		sb.append("<result>");
+		sb.append("<code>error</code>");
+		sb.append("<data>" + msg + "</data>");
+		sb.append("</result>");
+		return sb.toString();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
