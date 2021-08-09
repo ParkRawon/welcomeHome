c64e9d646b95c95a3bc673661ee3ba8c006c16e6
DESKTOP-7UNL8CG\admin <dkw0219753@daum.net>
2021-08-09 18:22:27 +0900

    8/9

---
+++ welcomeHome/src/main/java/co/yedam/shop/ShopDAO.java
@@ -0,0 +1,65 @@
+package co.yedam.shop;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import co.yedam.board.DAO;
+
+public class ShopDAO extends DAO {
+
+	//한건조회
+	public Item getItemNo(String no) {
+		connect();
+		Item item = null;
+		try {
+			pstmt = conn.prepareStatement("select * from shop where item_no = ?");
+			pstmt.setString(1, no);
+			rs = pstmt.executeQuery();			
+			if(rs.next()) {
+				item = new Item();
+				item.setItemNo(rs.getString("item_no"));
+				item.setItemName(rs.getString("item_name"));
+				item.setItemDesc(rs.getString("item_desc"));
+				item.setLikeIt(rs.getDouble("like_it"));
+				item.setPrice(rs.getDouble("price"));
+				item.setPriceOff(rs.getDouble("price_off"));
+				item.setImage(rs.getString("image"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return item;
+	}
+	
+	
+	public List<Item> getItemList() {  //전체 데이터 가져오기
+		List<Item> list = new ArrayList<>();
+
+		connect();
+		try {
+			pstmt = conn.prepareStatement("select * from shop");
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Item item = new Item();
+				item.setItemNo(rs.getString("item_no"));
+				item.setItemName(rs.getString("item_name"));
+				item.setItemDesc(rs.getString("item_desc"));
+				item.setLikeIt(rs.getDouble("like_it"));
+				item.setPrice(rs.getDouble("price"));
+				item.setPriceOff(rs.getDouble("price_off"));
+				item.setImage(rs.getString("image"));
+
+				list.add(item);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return list;
+	}
+
+}
